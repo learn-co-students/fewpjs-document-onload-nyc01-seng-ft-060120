fa4a1bc6ba43642f35d9e7f487e792eb9d69fa60
scubaguba345 <brandon.gubitosa@gmail.com>
2020-07-13 17:57:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    changeDom()
+  });
+
+  function changeDom() {
+      document.getElementById("text")
+      .textContent = "This is really cool!";
+  }
